8346cc7a237965dfab505eab3db321b4911f473b
kelson42 <kelson42@users.sourceforge.net>
2009-11-14 16:07:01 +0000

    + imp version

--- src/common/unaccent.cpp
+++ src/common/unaccent.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 
 /* Remove accent */
-std::string removeAccents(const char *text) { 
+std::string removeAccents(const char *text = NULL) { 
   char* out = 0;
   size_t out_length = 0;
-  std::string textWithoutAccent = text;
 
   if (!unac_string("UTF8", text, strlen(text), &out, &out_length)) {
+    std::string textWithoutAccent = text;
     textWithoutAccent = string(out, out_length);
     free(out);
+    return textWithoutAccent;
+  } if (text != NULL) {
+    return string(text);
   }
 
-  return textWithoutAccent;
+  return "";
 }
