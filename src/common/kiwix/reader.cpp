4b6caa48ce08dd432eac9a2cb50e970ec6ffc9ff
kelson42 <kelson42@users.sourceforge.net>
2011-07-27 19:25:25 +0000

    + few additional check to avoid segfaults if ZIM file is not there

--- src/common/kiwix/reader.cpp
+++ src/common/kiwix/reader.cpp
@@ -206,88 +206,88 @@ namespace kiwix {
   /* Get a content from a zim file */
   bool Reader::getContentByUrl(const string &urlStr, string &content, unsigned int &contentLength, string &contentType) {
     bool retVal = false;
-    const char *url = urlStr.c_str();
-    
-    /* Offset to visit the url */
-    unsigned int urlLength = strlen(url);
-    unsigned int offset = 0;
-    
-    /* Ignore the '/' */
-    while ((offset < urlLength) && (url[offset] == '/')) offset++;
-    
-    /* Get namespace */
-    char ns[1024];
-    unsigned int nsOffset = 0;
-    while ((offset < urlLength) && (url[offset] != '/')) {
-      ns[nsOffset] = url[offset];
-      offset++;
-      nsOffset++;
-    }
-    ns[nsOffset] = 0;
-    
-    /* Ignore the '/' */
-    while ((offset < urlLength) && (url[offset] == '/')) offset++;  
-    
-    /* Get content title */
-    char title[1024];
-    unsigned int titleOffset = 0;
-    while (offset < urlLength) {
-      title[titleOffset] = url[offset];
-      offset++;
-      titleOffset++;
-    }
-    title[titleOffset] = 0;
-    
-    /* unescape url */
-    string titleStr = string(title);
-    unescapeUrl(titleStr);
-    
-    /* Main page */
-    if (titleStr == "" && strcmp(ns, "") == 0) {
-      if (zimFileHandler->getFileheader().hasMainPage()) {
-	zim::Article article = zimFileHandler->getArticle(zimFileHandler->getFileheader().getMainPage());
-	ns[0] = article.getNamespace();
-	titleStr = article.getUrl();
-      }
-    }
-    
-    /* Extract the content from the zim file */
-    std::pair<bool, zim::File::const_iterator> resultPair = zimFileHandler->findx(ns[0], titleStr);
-    
-    /* Test if the article was found */
-    if (resultPair.first == true) {
-      
-      /* Get the article */
-      zim::Article article = zimFileHandler->getArticle(resultPair.second.getIndex());
-      
-      /* If redirect */
-      unsigned int loopCounter = 0;
-      while (article.isRedirect() && loopCounter++<42) {
-	article = article.getRedirectArticle();
-      }
-      
-      /* Get the content mime-type */
-      contentType = string(article.getMimeType().data(), article.getMimeType().size()); 
-      
-      /* Get the data */
-      content = string(article.getData().data(), article.getArticleSize());
+    content="";
+    contentType="";
+    contentLength = 0;
 
-      /* Try to set a stub HTML header/footer if necesssary */
-      if (contentType == "text/html" && std::string::npos == content.find("<body>")) {
-	content = "<html><head><title>" + article.getTitle() + "</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /></head><body>" + content + "</body></html>";
+    if (this->zimFileHandler != NULL) {
+      const char *url = urlStr.c_str();
+      
+      /* Offset to visit the url */
+      unsigned int urlLength = strlen(url);
+      unsigned int offset = 0;
+      
+      /* Ignore the '/' */
+      while ((offset < urlLength) && (url[offset] == '/')) offset++;
+      
+      /* Get namespace */
+      char ns[1024];
+      unsigned int nsOffset = 0;
+      while ((offset < urlLength) && (url[offset] != '/')) {
+	ns[nsOffset] = url[offset];
+	offset++;
+	nsOffset++;
+      }
+      ns[nsOffset] = 0;
+      
+      /* Ignore the '/' */
+      while ((offset < urlLength) && (url[offset] == '/')) offset++;  
+      
+      /* Get content title */
+      char title[1024];
+      unsigned int titleOffset = 0;
+      while (offset < urlLength) {
+	title[titleOffset] = url[offset];
+	offset++;
+	titleOffset++;
+      }
+      title[titleOffset] = 0;
+      
+      /* unescape url */
+      string titleStr = string(title);
+      unescapeUrl(titleStr);
+      
+      /* Main page */
+      if (titleStr == "" && strcmp(ns, "") == 0) {
+	if (zimFileHandler->getFileheader().hasMainPage()) {
+	  zim::Article article = zimFileHandler->getArticle(zimFileHandler->getFileheader().getMainPage());
+	  ns[0] = article.getNamespace();
+	  titleStr = article.getUrl();
+	}
       }
       
-      /* Get the data length */
-      contentLength = article.getArticleSize();
-
-      /* Set return value */
-      retVal = true;
-    } else {
-      /* The found article is not the good one */
-      content="";
-      contentType="";
-      contentLength = 0;
-      retVal = false;
+      /* Extract the content from the zim file */
+      std::pair<bool, zim::File::const_iterator> resultPair = zimFileHandler->findx(ns[0], titleStr);
+      
+      /* Test if the article was found */
+      if (resultPair.first == true) {
+	
+	/* Get the article */
+	zim::Article article = zimFileHandler->getArticle(resultPair.second.getIndex());
+	
+	/* If redirect */
+	unsigned int loopCounter = 0;
+	while (article.isRedirect() && loopCounter++<42) {
+	  article = article.getRedirectArticle();
+	}
+	
+	/* Get the content mime-type */
+	contentType = string(article.getMimeType().data(), article.getMimeType().size()); 
+	
+	/* Get the data */
+	content = string(article.getData().data(), article.getArticleSize());
+	
+	/* Try to set a stub HTML header/footer if necesssary */
+	if (contentType == "text/html" && std::string::npos == content.find("<body>")) {
+	  content = "<html><head><title>" + article.getTitle() + "</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /></head><body>" + content + "</body></html>";
+	}
+	
+	/* Get the data length */
+	contentLength = article.getArticleSize();
+	
+	/* Set return value */
+	retVal = true;
+      }
     }
     
     return retVal;
