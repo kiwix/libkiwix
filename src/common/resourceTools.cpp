f29a845682070b703df54f83dc5909332c5f9df6
kelson42 <kelson42@users.sourceforge.net>
2012-05-19 19:16:26 +0000

    + realloc values before creating the string

--- src/common/resourceTools.cpp
+++ src/common/resourceTools.cpp
@@ -4,7 +4,7 @@
 std::string getResourceAsString(const std::string &name) {
   std::map<std::string, const char*>::iterator it = resourceMap.find(name);
   if (it != resourceMap.end()) {
-    return std::string(resourceMap[name]);
+    return std::string(strdup(resourceMap[name]));
   }
   return "";
 }
