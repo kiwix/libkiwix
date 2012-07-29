f4dc0a43e04fa9debf544c615fe707422cf3496b
kelson42 <kelson42@users.sourceforge.net>
2012-07-29 22:56:07 +0000

    * FIXED: kiwix-serve memory leak on ARM (ID: 3538663)

--- src/common/resourceTools.cpp
+++ src/common/resourceTools.cpp
@@ -4,7 +4,7 @@
 std::string getResourceAsString(const std::string &name) {
   std::map<std::string, std::pair<const unsigned char*, unsigned int> >::iterator it = resourceMap.find(name);
   if (it != resourceMap.end()) {
-    return std::string(strdup((const char*)resourceMap[name].first), resourceMap[name].second);
+    return std::string((const char*)resourceMap[name].first, resourceMap[name].second);
   }
   return "";
 }
