654a8e304c3db867822ada9578984171593d85a8
Matthieu Gautier <mgautier@kymeria.fr>
2017-12-19 14:10:56 +0100

    Set the verbosity of the underlying zim::Search.

--- src/searcher.cpp
+++ src/searcher.cpp
@@ -136,6 +136,7 @@ void Searcher::search(std::string& search,
       }
     }
     zim::Search* search = new zim::Search(zims);
+    search->set_verbose(verbose);
     search->set_query(unaccentedSearch);
     search->set_range(resultStart, resultEnd);
     internal->_search = search;
@@ -190,6 +191,7 @@ void Searcher::geo_search(float latitude, float longitude, float distance,
     zims.push_back((*current)->getZimFileHandler());
   }
   zim::Search* search = new zim::Search(zims);
+  search->set_verbose(verbose);
   search->set_query("");
   search->set_georange(latitude, longitude, distance);
   search->set_range(resultStart, resultEnd);
@@ -244,6 +246,7 @@ void Searcher::suggestions(std::string& searchPattern, const bool verbose)
     zims.push_back((*current)->getZimFileHandler());
   }
   zim::Search* search = new zim::Search(zims);
+  search->set_verbose(verbose);
   search->set_query(unaccentedSearch);
   search->set_range(resultStart, resultEnd);
   search->set_suggestion_mode(true);
