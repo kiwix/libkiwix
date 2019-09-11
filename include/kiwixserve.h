e42e061d45763a8c691da8a86ef32bb0f6857b10
Matthieu Gautier <mgautier@kymeria.fr>
2019-09-11 11:30:59 +0200

    Add a way to specify a library to use with kiwix-serve.
    If kiwix-desktop use a `library.xml` in the same directory than the
    executable, we need to use it instead of the default one.
    
    Instead of detect again the `library.xml` to use, let `kiwix-desktop` set
    the library to use.
    
    This also fix a issue when `/` is not a valid path separator in windows.

--- include/kiwixserve.h
+++ include/kiwixserve.h
@@ -2,6 +2,7 @@
 #define KIWIXLIB_KIWIX_SERVE_H_
 
 #include <memory>
+#include <string>
 
 class Subprocess;
 namespace kiwix {
@@ -9,7 +10,7 @@ namespace kiwix {
 class KiwixServe
 {
   public:
-    KiwixServe(int port = 8181);
+    KiwixServe(const std::string& libraryPath, int port = 8181);
     ~KiwixServe();
 
     void run();
@@ -20,6 +21,7 @@ class KiwixServe
   private:
   std::unique_ptr<Subprocess> mp_kiwixServe;
   int m_port;
+  std::string m_libraryPath;
 };
 
 }; //end namespace kiwix
