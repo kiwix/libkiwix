a2c2955f4152b1895c2db9b58dfe51ddc5c9bcad
luddens <pierre.luddens@epitech.eu>
2019-10-22 11:26:41 +0200

    add kiwixserve unit test

---
+++ test/kiwixserve.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "../include/kiwixserve.h"
+
+TEST(KiwixServeTest, PortTest)
+{
+    kiwix::KiwixServe kiwixServe("libraryPath", 8181);
+    EXPECT_EQ(kiwixServe.getPort(), 8181);
+    kiwixServe.setPort(8484);
+    EXPECT_EQ(kiwixServe.getPort(), 8484);
+    EXPECT_EQ(kiwixServe.setPort(0), -1);
+    EXPECT_EQ(kiwixServe.setPort(3456789), -1);    
+}
+
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
