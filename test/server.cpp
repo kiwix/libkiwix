6ec378cd5568b70eca094bfb3946808561fb6e95
Veloman Yunkan <veloman.yunkan@gmail.com>
2020-04-15 12:30:50 +0400

    Cleaned up the Server unit-test
    - Encapsulated the HTTP client inside ZimFileServer
    
    - A second server is created only for the test case requiring a second
      server

--- test/server.cpp
+++ test/server.cpp
@@ -15,23 +15,39 @@ bool is_valid_etag(const std::string& etag)
 
 class ZimFileServer
 {
-public:
-  ZimFileServer(std::string address, int serverPort, std::string zimpath);
+public: // types
+  typedef httplib::Headers                    Headers;
+  typedef std::shared_ptr<httplib::Response>  Response;
+
+public: // functions
+  ZimFileServer(int serverPort, std::string zimpath);
   ~ZimFileServer();
 
-private:
+  Response GET(const char* path, const Headers& headers = Headers())
+  {
+    return client->Get(path, headers);
+  }
+
+  Response HEAD(const char* path, const Headers& headers = Headers())
+  {
+    return client->Head(path, headers);
+  }
+
+private: // data
   kiwix::Library library;
   kiwix::Manager manager;
   std::unique_ptr<kiwix::HumanReadableNameMapper> nameMapper;
   std::unique_ptr<kiwix::Server> server;
+  std::unique_ptr<httplib::Client> client;
 };
 
-ZimFileServer::ZimFileServer(std::string address, int serverPort, std::string zimpath)
+ZimFileServer::ZimFileServer(int serverPort, std::string zimpath)
 : manager(&this->library)
 {
   if (!manager.addBookFromPath(zimpath, zimpath, "", false))
     throw std::runtime_error("Unable to add the ZIM file '" + zimpath + "'");
 
+  const std::string address = "127.0.0.1";
   nameMapper.reset(new kiwix::HumanReadableNameMapper(library, false));
   server.reset(new kiwix::Server(&library, nameMapper.get()));
   server->setAddress(address);
@@ -41,6 +57,8 @@ ZimFileServer::ZimFileServer(std::string address, int serverPort, std::string zi
 
   if ( !server->start() )
     throw std::runtime_error("ZimFileServer failed to start");
+
+  client.reset(new httplib::Client(address, serverPort));
 }
 
 ZimFileServer::~ZimFileServer()
@@ -52,16 +70,13 @@ class ServerTest : public ::testing::Test
 {
 protected:
   std::unique_ptr<ZimFileServer>   zfs1_;
-  std::unique_ptr<ZimFileServer>   zfs2_;
-  std::unique_ptr<httplib::Client> client1_;
-  std::unique_ptr<httplib::Client> client2_;
+
+  const int PORT = 8001;
+  const std::string ZIMFILE = "zimfile.zim";
 
 protected:
   void SetUp() override {
-    zfs1_.reset(new ZimFileServer("127.0.0.1", 8001, "zimfile.zim"));
-    zfs2_.reset(new ZimFileServer("127.0.0.1", 8002, "zimfile.zim"));
-    client1_.reset(new httplib::Client("127.0.0.1", 8001));
-    client2_.reset(new httplib::Client("127.0.0.1", 8002));
+    zfs1_.reset(new ZimFileServer(PORT, ZIMFILE));
   }
 
   void TearDown() override {
@@ -71,58 +86,59 @@ protected:
 
 TEST_F(ServerTest, HeadMethodIsSupported)
 {
-  ASSERT_EQ(200, client1_->Head("/")->status);
+  ASSERT_EQ(200, zfs1_->HEAD("/")->status);
 }
 
 TEST_F(ServerTest, ETagHeaderIsSet)
 {
-  const auto responseToGet = client1_->Get("/");
+  const auto responseToGet = zfs1_->GET("/");
   EXPECT_TRUE(responseToGet->has_header("ETag"));
   EXPECT_TRUE(is_valid_etag(responseToGet->get_header_value("ETag")));
 
-  const auto responseToHead = client1_->Head("/");
+  const auto responseToHead = zfs1_->HEAD("/");
   EXPECT_TRUE(responseToHead->has_header("ETag"));
   EXPECT_TRUE(is_valid_etag(responseToHead->get_header_value("ETag")));
 }
 
 TEST_F(ServerTest, EtagIsTheSameInResponsesToDifferentRequestsOfTheSameURL)
 {
-  const auto h1 = client1_->Head("/");
-  const auto h2 = client1_->Head("/");
+  const auto h1 = zfs1_->HEAD("/");
+  const auto h2 = zfs1_->HEAD("/");
   ASSERT_EQ(h1->get_header_value("ETag"), h2->get_header_value("ETag"));
 }
 
 TEST_F(ServerTest, EtagIsTheSameAcrossHeadAndGet)
 {
-  const auto g = client1_->Get("/");
-  const auto h = client1_->Head("/");
+  const auto g = zfs1_->GET("/");
+  const auto h = zfs1_->HEAD("/");
   ASSERT_EQ(h->get_header_value("ETag"), g->get_header_value("ETag"));
 }
 
 TEST_F(ServerTest, DifferentServerInstancesProduceDifferentETags)
 {
-  const auto h1 = client1_->Head("/");
-  const auto h2 = client2_->Head("/");
+  ZimFileServer zfs2(PORT + 1, ZIMFILE);
+  const auto h1 = zfs1_->HEAD("/");
+  const auto h2 = zfs2.HEAD("/");
   ASSERT_NE(h1->get_header_value("ETag"), h2->get_header_value("ETag"));
 }
 
 TEST_F(ServerTest, IfNoneMatchRequestsWithMatchingEtagResultIn304Responses)
 {
-  const auto g = client1_->Get("/");
+  const auto g = zfs1_->GET("/");
   const auto etag = g->get_header_value("Etag");
-  const auto h = client1_->Head("/", { {"If-None-Match", etag} } );
-  const auto g2 = client1_->Get("/", { {"If-None-Match", etag} } );
+  const auto h = zfs1_->HEAD("/", { {"If-None-Match", etag} } );
+  const auto g2 = zfs1_->GET("/", { {"If-None-Match", etag} } );
   EXPECT_EQ(304, h->status);
   EXPECT_EQ(304, g2->status);
 }
 
 TEST_F(ServerTest, IfNoneMatchRequestsWithMismatchingEtagResultIn200Responses)
 {
-  const auto g = client1_->Get("/");
+  const auto g = zfs1_->GET("/");
   const auto etag = g->get_header_value("Etag");
   const auto etag2 = etag.substr(0, etag.size() - 1) + "x\"";
-  const auto h = client1_->Head("/", { {"If-None-Match", etag2} } );
-  const auto g2 = client1_->Get("/", { {"If-None-Match", etag2} } );
+  const auto h = zfs1_->HEAD("/", { {"If-None-Match", etag2} } );
+  const auto g2 = zfs1_->GET("/", { {"If-None-Match", etag2} } );
   EXPECT_EQ(200, h->status);
   EXPECT_EQ(200, g2->status);
 }
