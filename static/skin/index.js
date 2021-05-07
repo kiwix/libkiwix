1dbed284993779f7ee3d6da67d0f694069712bf0
Manan Jethwani <mananjethwani02@gmail.com>
2021-05-07 16:59:26 +0530

    added dynamic loading of data using OPDS

---
+++ static/skin/index.js
@@ -0,0 +1,32 @@
+window.onload = async (event) => {
+    const root = $( `link[type='root']` ).attr("href");
+    await fetch(`${root}/catalog/search`)
+    .then(async (resp) => {
+        const data = new window.DOMParser().parseFromString(await resp.text(), 'application/xml');
+        const entries = data.querySelectorAll("entry");
+        displayBooks(entries);
+    });
+};
+
+
+function getInnerHtml(node, query) {
+    return node.querySelector(query).innerHTML;
+}
+
+function displayBooks(books) {
+    let bookHtml = '';
+    books.forEach((book) => {
+        const link = book.querySelector('link').getAttribute('href');
+        const title = getInnerHtml(book, 'title');
+        const description = getInnerHtml(book, 'summary');
+        
+        bookHtml += `<a href='${link}'><div class='book'>
+            <div class='book__background' style="background-image: url('${getInnerHtml(book, 'icon')}');">
+            <div class='book__title' title='${title}'>${title}</div>
+            <div class='book__description' title='${description}'>${description}</div>
+            <div class='book__info'>${getInnerHtml(book, 'articleCount')} articles, ${getInnerHtml(book, 'mediaCount')} medias</div>
+            </div>
+        </div></a>`;
+    });
+    document.querySelector('.book__list').innerHTML = bookHtml;
+}
