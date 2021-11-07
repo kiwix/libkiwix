4f65811011469465ddb1b0809e971d8fb3997b02
Veloman Yunkan <veloman.yunkan@gmail.com>
2021-11-07 11:08:12 +0400

    Moved Book.updateTest below Book.updateFromXMLTest
    Book.updateTest is going to be modified so that it relies on
    functionality tested by Book.updateFromXMLTest. Hence the order of the
    tests better reflect that dependency.

--- test/book.cpp
+++ test/book.cpp
@@ -2,42 +2,6 @@
 #include "../include/book.h"
 #include <pugixml.hpp>
 
-TEST(BookTest, updateTest)
-{
-    kiwix::Book book;
-
-    book.setId("xyz");
-    book.setReadOnly(false);
-    book.setPath("/home/user/Downloads/skin-of-color-society_en_all_2019-11.zim");
-    book.setPathValid(true);
-    book.setUrl("book-url");
-    book.setTags("youtube;_videos:yes;_ftindex:yes;_ftindex:yes;_pictures:yes;_details:yes");
-    book.setName("skin-of-color-society_en_all");
-    book.setFavicon("book-favicon");
-    book.setFaviconMimeType("book-favicon-mimetype");
-
-    kiwix::Book newBook;
-
-    newBook.setReadOnly(true);
-    EXPECT_FALSE(newBook.update(book));
-
-    newBook.setReadOnly(false);
-    EXPECT_FALSE(newBook.update(book));
-
-    newBook.setId("xyz");
-    EXPECT_TRUE(newBook.update(book));
-
-    EXPECT_EQ(newBook.readOnly(), book.readOnly());
-    EXPECT_EQ(newBook.getPath(), book.getPath());
-    EXPECT_EQ(newBook.isPathValid(), book.isPathValid());
-    EXPECT_EQ(newBook.getUrl(), book.getUrl());
-    EXPECT_EQ(newBook.getTags(), book.getTags());
-    EXPECT_EQ(newBook.getCategory(), book.getCategory());
-    EXPECT_EQ(newBook.getName(), book.getName());
-    EXPECT_EQ(newBook.getFavicon(), book.getFavicon());
-    EXPECT_EQ(newBook.getFaviconMimeType(), book.getFaviconMimeType());
-}
-
 namespace
 {
 
@@ -171,4 +135,40 @@ TEST(BookTest, updateCopiesCategory)
     EXPECT_EQ(newBook.getCategory(), "");
     newBook.update(book);
     EXPECT_EQ(newBook.getCategory(), "ted");
+}
+
+TEST(BookTest, updateTest)
+{
+    kiwix::Book book;
+
+    book.setId("xyz");
+    book.setReadOnly(false);
+    book.setPath("/home/user/Downloads/skin-of-color-society_en_all_2019-11.zim");
+    book.setPathValid(true);
+    book.setUrl("book-url");
+    book.setTags("youtube;_videos:yes;_ftindex:yes;_ftindex:yes;_pictures:yes;_details:yes");
+    book.setName("skin-of-color-society_en_all");
+    book.setFavicon("book-favicon");
+    book.setFaviconMimeType("book-favicon-mimetype");
+
+    kiwix::Book newBook;
+
+    newBook.setReadOnly(true);
+    EXPECT_FALSE(newBook.update(book));
+
+    newBook.setReadOnly(false);
+    EXPECT_FALSE(newBook.update(book));
+
+    newBook.setId("xyz");
+    EXPECT_TRUE(newBook.update(book));
+
+    EXPECT_EQ(newBook.readOnly(), book.readOnly());
+    EXPECT_EQ(newBook.getPath(), book.getPath());
+    EXPECT_EQ(newBook.isPathValid(), book.isPathValid());
+    EXPECT_EQ(newBook.getUrl(), book.getUrl());
+    EXPECT_EQ(newBook.getTags(), book.getTags());
+    EXPECT_EQ(newBook.getCategory(), book.getCategory());
+    EXPECT_EQ(newBook.getName(), book.getName());
+    EXPECT_EQ(newBook.getFavicon(), book.getFavicon());
+    EXPECT_EQ(newBook.getFaviconMimeType(), book.getFaviconMimeType());
 }
