8bdcb90818b4e5720ce98d37cdf51f19d684cc49
Nikhil Tanwar <2002nikhiltanwar@gmail.com>
2022-01-01 20:00:04 +0530

    Make aria2 secret a random value
    Apps using this service will not have a default aria secret (previously 'kiwixariarpc')

--- src/aria2.h
+++ src/aria2.h
@@ -37,6 +37,7 @@ class Aria2
 
     std::string addUri(const std::vector<std::string>& uri, const std::vector<std::pair<std::string, std::string>>& options = {});
     std::string tellStatus(const std::string& gid, const std::vector<std::string>& statusKey);
+    static std::string getNewRpcSecret();
     std::vector<std::string> tellActive();
     std::vector<std::string> tellWaiting();
     void saveSession();
