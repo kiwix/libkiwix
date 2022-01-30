ed46541b6f2a0db272d365b5aa0e0e42aa9ba8c0
Veloman Yunkan <veloman.yunkan@gmail.com>
2022-01-30 13:10:57 +0400

    Clean-up promised in the previous commit

--- test/server.cpp
+++ test/server.cpp
@@ -369,34 +369,16 @@ ExpectedResponseData operator&&(const ExpectedResponseData& a,
   };
 }
 
-class TestContentIn404HtmlResponse
+class TestContentIn404HtmlResponse : public ExpectedResponseData
 {
 public:
   TestContentIn404HtmlResponse(const std::string& url,
                                const ExpectedResponseData& erd)
-    : url(url)
-    , bookName(erd.bookName)
-    , bookTitle(erd.bookTitle)
-    , expectedBody(erd.expectedBody)
+    : ExpectedResponseData(erd)
+    , url(url)
   {}
 
-  TestContentIn404HtmlResponse(const std::string& url,
-                               const std::string& expectedBody)
-    : url(url)
-    , expectedBody(expectedBody)
-  {}
-
-  TestContentIn404HtmlResponse(const std::string& url,
-                               const std::string& bookName,
-                               const std::string& bookTitle,
-                               const std::string& expectedBody)
-    : url(url)
-    , bookName(bookName)
-    , bookTitle(bookTitle)
-    , expectedBody(expectedBody)
-  {}
-
-  const std::string url, bookName, bookTitle, expectedBody;
+  const std::string url;
 
   std::string expectedResponse() const;
 
