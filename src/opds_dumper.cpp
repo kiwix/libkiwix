4bd02f07eb3fa391dd40044529312c3a86ef22e4
Emmanuel Engelhart <kelson@kiwix.org>
2022-03-05 16:56:21 +0100

    Beautify slightly the code

--- src/opds_dumper.cpp
+++ src/opds_dumper.cpp
@@ -127,43 +127,43 @@ BooksData getBooksData(const Library* library, const std::vector<std::string>& b
 }
 
 std::map<std::string, std::string> iso639_3 = {
-  {"atj","atikamekw"},
-  {"azb","آذربایجان دیلی"},
-  {"bcl","central bikol"},
-  {"bgs","tagabawa"},
-  {"bxr","буряад хэлэн"},
-  {"cbk","chavacano"},
-  {"cdo","閩東語"},
-  {"diq","dimli"},
-  {"dty","डोटेली"},
-  {"eml","emiliân-rumagnōl"},
-  {"fbs","српскохрватски"},
-  {"kbp","kabɩyɛ"},
-  {"kld","Gamilaraay"},
-  {"lbe","лакку маз"},
-  {"lbj","ལ་དྭགས་སྐད་"},
-  {"map","Austronesian"},
-  {"mhr","марий йылме"},
-  {"mnw","ဘာသာမန်"},
-  {"myn","mayan"},
-  {"nah","nahuatl"},
-  {"nai","north American Indian"},
-  {"nds","plattdütsch"},
-  {"nrm","bhasa narom"},
-  {"olo","livvi"},
-  {"pih","Pitcairn-Norfolk"},
-  {"pnb","Western Panjabi"},
-  {"rmr","Caló"},
-  {"rmy","romani shib"},
-  {"roa","romance languages"},
-  {"twi","twi"},
-  {"ido","ido"},
-  {"dag", "Dagbani"}
+  {"atj", "atikamekw"},
+  {"azb", "آذربایجان دیلی"},
+  {"bcl", "central bikol"},
+  {"bgs", "tagabawa"},
+  {"bxr", "буряад хэлэн"},
+  {"cbk", "chavacano"},
+  {"cdo", "閩東語"},
+  {"dag", "Dagbani"},
+  {"diq", "dimli"},
+  {"dty", "डोटेली"},
+  {"eml", "emiliân-rumagnōl"},
+  {"fbs", "српскохрватски"},
+  {"ido", "ido"},
+  {"kbp", "kabɩyɛ"},
+  {"kld", "Gamilaraay"},
+  {"lbe", "лакку маз"},
+  {"lbj", "ལ་དྭགས་སྐད་"},
+  {"map", "Austronesian"},
+  {"mhr", "марий йылме"},
+  {"mnw", "ဘာသာမန်"},
+  {"myn", "mayan"},
+  {"nah", "nahuatl"},
+  {"nai", "north American Indian"},
+  {"nds", "plattdütsch"},
+  {"nrm", "bhasa narom"},
+  {"olo", "livvi"},
+  {"pih", "Pitcairn-Norfolk"},
+  {"pnb", "Western Panjabi"},
+  {"rmr", "Caló"},
+  {"rmy", "romani shib"},
+  {"roa", "romance languages"},
+  {"twi", "twi"}
 };
 
 std::once_flag fillLanguagesFlag;
 
-void fillLanguagesMap() 
+void fillLanguagesMap()
 {
   for (auto icuLangPtr = icu::Locale::getISOLanguages(); *icuLangPtr != NULL; ++icuLangPtr) {
     auto lang = *icuLangPtr;
