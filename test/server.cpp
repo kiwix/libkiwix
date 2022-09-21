defa38719d6f3a0efe0f276deb72574d31ace119
Veloman Yunkan <veloman.yunkan@gmail.com>
2022-09-21 15:44:09 +0400

    Fix cacheids after a rebase
    A rebase invalidated the cacheids in the previous commits of the
    iframe_based_content_viewer branch. This commit fixes only the current
    state leaving the history with wrong cacheids - this can be an issue
    for `git bisect` being executed on a commit range overlapping with
    the iframe_based_content_viewer branch.

--- test/server.cpp
+++ test/server.cpp
@@ -197,9 +197,9 @@ R"EXPECTEDRESULT(                                <img src="../skin/download.png?
     },
     {
       /* url */ "/ROOT/viewer",
-R"EXPECTEDRESULT(    <link type="text/css" href="./skin/taskbar.css?cacheid=26082885" rel="Stylesheet" />
+R"EXPECTEDRESULT(    <link type="text/css" href="./skin/taskbar.css?cacheid=216d6b5d" rel="Stylesheet" />
     <link type="text/css" href="./skin/css/autoComplete.css?cacheid=08951e06" rel="Stylesheet" />
-    <script type="text/javascript" src="./skin/viewer.js?cacheid=9ec149c0" defer></script>
+    <script type="text/javascript" src="./skin/viewer.js?cacheid=9a336712" defer></script>
     <script type="text/javascript" src="./skin/autoComplete.min.js?cacheid=1191aaaf"></script>
       const blankPageUrl = `${root}/skin/blank.html`;
             <label for="kiwix_button_show_toggle"><img src="./skin/caret.png?cacheid=22b942b4" alt=""></label>
