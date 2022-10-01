ffbda34b7517c3cf3a8cdc97a9a1d35c155c5860
Emmanuel Engelhart <kelson@kiwix.org>
2022-10-01 21:15:50 +0200

    Fix: improvement to handle dirs with spaces

--- scripts/format_code.sh
+++ scripts/format_code.sh
@@ -3,9 +3,10 @@
 # Compute 'src' path
 SCRIPT_DIR=$(dirname "$0")
 REPO_DIR=$(readlink -f "$SCRIPT_DIR"/..)
-DIRS="$REPO_DIR/src $REPO_DIR/include"
+DIRS="src include"
 
 # Apply formating to all *.cpp and *.h files
+cd "$REPO_DIR"
 for FILE in $(find $DIRS -name '*.h' -o -name '*.cpp')
 do
   echo $FILE
