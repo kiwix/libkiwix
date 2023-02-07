4bf4b66b27ab8e0c7b7aa0f999fecb4af45cfa17
Veloman Yunkan <veloman.yunkan@gmail.com>
2023-02-07 16:07:58 +0100

    Explicitly styled UI language selector
    The recently introduced ZIM viewer UI language selector looked
    adequately nice under Firefox without any explicit styling applied.
    Under SeaMonkey, however, its default look and feel was intolerable, so
    I used this opportunity to make the UI language selector comply with the
    current fashion of the ZIM viewer toolbar.

--- test/server.cpp
+++ test/server.cpp
@@ -71,7 +71,7 @@ const ResourceCollection resources200Compressible{
   { DYNAMIC_CONTENT, "/ROOT/skin/mustache.min.js" },
   { STATIC_CONTENT,  "/ROOT/skin/mustache.min.js?cacheid=bd23c4fb" },
   { DYNAMIC_CONTENT, "/ROOT/skin/taskbar.css" },
-  { STATIC_CONTENT,  "/ROOT/skin/taskbar.css?cacheid=2cbac34b" },
+  { STATIC_CONTENT,  "/ROOT/skin/taskbar.css?cacheid=eb3bec90" },
   { DYNAMIC_CONTENT, "/ROOT/skin/viewer.js" },
   { STATIC_CONTENT,  "/ROOT/skin/viewer.js?cacheid=03fd97ee" },
   { DYNAMIC_CONTENT, "/ROOT/skin/fonts/Poppins.ttf" },
@@ -298,7 +298,7 @@ R"EXPECTEDRESULT(                                <img src="../skin/download.png?
     },
     {
       /* url */ "/ROOT/viewer",
-R"EXPECTEDRESULT(    <link type="text/css" href="./skin/taskbar.css?cacheid=2cbac34b" rel="Stylesheet" />
+R"EXPECTEDRESULT(    <link type="text/css" href="./skin/taskbar.css?cacheid=eb3bec90" rel="Stylesheet" />
     <link type="text/css" href="./skin/css/autoComplete.css?cacheid=08951e06" rel="Stylesheet" />
     <script type="module" src="./skin/i18n.js?cacheid=6da2bca0" defer></script>
     <script type="text/javascript" src="./skin/languages.js?cacheid=fe100348" defer></script>
