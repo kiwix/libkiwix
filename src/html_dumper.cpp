6071b98fb730e6f52a59e3550535803a67b5f85d
Nikhil Tanwar <2002nikhiltanwar@gmail.com>
2023-02-13 04:35:53 +0530

    Import book tiles
    Tries to copy the same design of tiles as main page with javascript enabled

--- src/html_dumper.cpp
+++ src/html_dumper.cpp
@@ -19,16 +19,24 @@ std::string HTMLDumper::dumpPlainHTML() const
 {
   kainjow::mustache::list booksData;
   for ( const auto& bookId : library->getBooksIds() ) {
-    const auto bookEx = library->getBookById(bookId);
-    const auto bookName = bookEx.getName();
+    const auto bookObj = library->getBookById(bookId);
+    const auto bookTitle = bookObj.getTitle();
+    const auto bookDescription = bookObj.getDescription();
+    const auto langCode = bookObj.getCommaSeparatedLanguages();
+    const auto bookIconUrl = rootLocation + "/catalog/v2/illustration/" + bookId +  "/?size=48";
+    std::string faviconAttr = "style=background-image:url(" + bookIconUrl + ")";
     booksData.push_back(kainjow::mustache::object{
-      {"name", bookName}
+      {"title", bookTitle},
+      {"description", bookDescription},
+      {"langCode", langCode},
+      {"faviconAttr", faviconAttr}
     });
   }
 
   return render_template(
              RESOURCE::templates::no_js_library_page_html,
              kainjow::mustache::object{
+               {"root", rootLocation},
                {"books", booksData }
              }
   );
