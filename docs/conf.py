0e8a2952d582023255da25e81571347b9dae0949
Matthieu Gautier <mgautier@kymeria.fr>
2023-08-24 10:45:23 +0200

    Always set html_theme in doc configuration.
    Lat version of read the doc do not set a html_theme for us.
    So we have to always set it.
    
    See readthedocs/readthedocs.org#10638

--- docs/conf.py
+++ docs/conf.py
@@ -24,8 +24,6 @@ author = 'libkiwix-team'
 
 # -- General configuration ---------------------------------------------------
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -42,9 +40,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-if not on_rtd:
-    html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
