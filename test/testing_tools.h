e481164258f706bac2bfbbac0d7599dd6a92a2cb
Veloman Yunkan <veloman.yunkan@gmail.com>
2026-02-22 11:34:03 +0400

    Extracted test/testing_tools.h

---
+++ test/testing_tools.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2026 Veloman Yunkan
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU  General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#ifndef LIBKIWIX_TESTING_TOOLS_H
+#define LIBKIWIX_TESTING_TOOLS_H
+
+#include <sstream>
+#include <iostream>
+
+namespace kiwix
+{
+
+namespace testing
+{
+
+class CapturedStderr
+{
+  std::ostringstream buffer;
+  std::streambuf* const sbuf;
+public:
+  CapturedStderr()
+    : sbuf(std::cerr.rdbuf())
+  {
+    std::cerr.rdbuf(buffer.rdbuf());
+  }
+
+  CapturedStderr(const CapturedStderr&) = delete;
+
+  ~CapturedStderr()
+  {
+    std::cerr.rdbuf(sbuf);
+  }
+
+  operator std::string() const { return buffer.str(); }
+};
+
+} // namespace testing
+
+} // namespace kiwix
+
+#endif // LIBKIWIX_TESTING_TOOLS_H
